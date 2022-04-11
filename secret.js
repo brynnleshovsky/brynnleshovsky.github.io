664f5dca12ba42344bbd86c33dd1f880b8c71363
Leshovsky Brynn <815990@stma.k12.mn.us>
2022-04-11 11:45:08 -0500

    save passsword

--- secret.js
+++ secret.js
@@ -1,7 +1,16 @@
-function check (){
+function checkcorrect (){
 if (document.getElementById("password").value == "FloridaFroggy"){
 	document.getElementById("access").innerHTML = "Access Granted!"
 }	
 }
 
-document.getElementById("check").addEventListener("click", check)
+document.getElementById("check").addEventListener("click", checkcorrect)
+
+
+function checkincorrect (){
+if (document.getElementById("password").value !== "FloridaFroggy"){
+	document.getElementById("access").innerHTML = "Access Denied!"
+}	
+}
+
+document.getElementById("check").addEventListener("click", checkincorrect)

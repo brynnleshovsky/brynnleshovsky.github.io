c7332399a96f5753c21aa16cb07e90df183a53b4
Leshovsky Brynn <815990@stma.k12.mn.us>
2022-04-25 12:45:11 -0500

    finish website

---
+++ homepage.js
@@ -0,0 +1,51 @@
+document.getElementById("activities").addEventListener("click", activities)
+function activities (){
+	window.open("https://stmaknights.com/")
+}
+
+
+document.getElementById("eventcalendar").addEventListener("click", eventcalendar)
+function eventcalendar (){
+	window.open("https://www.stma.k12.mn.us/Page/2#calendar1/20220425/month")
+}
+
+
+document.getElementById("yearlycalendar").addEventListener("click", yearlycalendar)
+function yearlycalendar (){
+	window.open("https://www.stma.k12.mn.us/domain/4555")
+}
+
+
+
+document.getElementById("studentvue").addEventListener("click", studentvue)
+function studentvue (){
+	window.open("https://pvue7.region1.k12.mn.us/rt0885/PXP2_Login.aspx")
+}
+
+
+
+document.getElementById("menus").addEventListener("click", menus)
+function menus (){
+	window.open("https://stma.nutrislice.com/")
+}
+
+
+
+document.getElementById("employment").addEventListener("click", employment)
+function employment (){
+	window.open("https://www.stma.k12.mn.us/domain/2433")
+}
+
+
+
+document.getElementById("skyward").addEventListener("click", skyward)
+function skyward (){
+	window.open("https://pac.stma.k12.mn.us/scripts/wsisa.dll/WService=wsFin/seplog01.w")
+}
+
+
+
+document.getElementById("email").addEventListener("click", email)
+function email (){
+	window.open("https://mail.google.com/mail/u/0/#inbox")
+}

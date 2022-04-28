c8ef1ab48e05adb440135bc592e485a348105a69
Leshovsky Brynn <815990@stma.k12.mn.us>
2022-04-28 12:27:01 -0500

    Apple day 3/4

--- apple.js
+++ apple.js
@@ -1,1 +1,7 @@
-// JavaScript Document
+var Store = document.getElementById("store");
+var Mac = document.getElementById("mac");
+var iPad = document.getElementById("iPad");
+var iPhone = document.getElementById("iPhone");
+var Watch = document.getElementById("Watch");
+var AirPods = document.getElementById("AirPods");
+var TVandHome = document.getElementById("TVandHome");

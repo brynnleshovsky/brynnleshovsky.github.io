3088e9ce400ad66765d372267811865d308a6de6
Leshovsky Brynn <815990@stma.k12.mn.us>
2022-04-29 12:16:21 -0500

    Finished Apple Website

--- apple.js
+++ apple.js
@@ -5,3 +5,173 @@ var iPhone = document.getElementById("iPhone");
 var Watch = document.getElementById("Watch");
 var AirPods = document.getElementById("AirPods");
 var TVandHome = document.getElementById("TVandHome");
+var OnlyonApple = document.getElementById("OnlyonApple");
+var Accessories = document.getElementById("Accessories");
+var Support = document.getElementById("Support");
+
+var pro = document.getElementById("pro");
+var thirteen = document.getElementById("thirteen");
+var SE = document.getElementById("SE");
+var twelve = document.getElementById("twelve");
+var eleven = document.getElementById("eleven");
+var Compare = document.getElementById("Compare");
+var AirPods1 = document.getElementById("AirPods1");
+var AirTag = document.getElementById("AirTag");
+var Accessories1 = document.getElementById("Accessories1");
+var AppleCard = document.getElementById("AppleCard");
+var iOS = document.getElementById("iOS");
+var Shop = document.getElementById("Shop");
+
+var learnmore1 = document.getElementById("learnmoreblue");
+var learnmore2 = document.getElementById("learnmore2");
+var button = document.getElementById("button");
+
+function store (){
+	window.open("https://www.apple.com/store");
+}
+Store.addEventListener("click", store);
+
+
+function mac (){
+	window.open("https://www.apple.com/mac/");
+}
+Mac.addEventListener("click", mac);
+
+
+function ipad (){
+	window.open("https://www.apple.com/ipad/");
+}
+iPad.addEventListener("click", ipad);
+
+
+function iphone (){
+	window.open("https://www.apple.com/iphone/");
+}
+iPhone.addEventListener("click", iphone);
+
+
+function watch (){
+	window.open("https://www.apple.com/watch/");
+}
+Watch.addEventListener("click", watch);
+
+
+function airPods (){
+	window.open("https://www.apple.com/airpods/");
+}
+AirPods.addEventListener("click", airPods);
+
+
+function tvandHome (){
+	window.open("https://www.apple.com/tv-home/");
+}
+TVandHome.addEventListener("click", tvandHome);
+
+
+function onlyonApple (){
+	window.open("https://www.apple.com/services/");
+}
+OnlyonApple.addEventListener("click", onlyonApple);
+
+
+function accessories (){
+	window.open("https://www.apple.com/shop/accessories/all");
+}
+Accessories.addEventListener("click", accessories);
+
+
+function support (){
+	window.open("https://support.apple.com/");
+}
+Support.addEventListener("click", support);
+
+
+
+
+
+function Pro (){
+	window.open("https://www.apple.com/iphone-13-pro/");
+}
+pro.addEventListener("click", Pro);
+
+
+function Thirteen (){
+	window.open("https://www.apple.com/iphone-13/");
+}
+thirteen.addEventListener("click", Thirteen);
+
+
+function se (){
+	window.open("https://www.apple.com/iphone-se/");
+}
+SE.addEventListener("click", se);
+
+
+function Twelve (){
+	window.open("https://www.apple.com/iphone-12/key-features/");
+}
+twelve.addEventListener("click", Twelve);
+
+
+function Eleven (){
+	window.open("https://www.apple.com/shop/buy-iphone/iphone-11");
+}
+eleven.addEventListener("click", Eleven);
+
+
+function compare (){
+	window.open("https://www.apple.com/iphone/compare/");
+}
+Compare.addEventListener("click", compare);
+
+
+function airPods1 (){
+	window.open("https://www.apple.com/airpods/");
+}
+AirPods1.addEventListener("click", airPods1);
+
+
+function airTag (){
+	window.open("https://www.apple.com/airtag/");
+}
+AirTag.addEventListener("click", airTag);
+
+
+function accessories1 (){
+	window.open("https://www.apple.com/shop/iphone/accessories");
+}
+Accessories1.addEventListener("click", accessories1);
+
+
+function appleCard (){
+	window.open("https://www.apple.com/shop/iphone/accessories");
+}
+AppleCard.addEventListener("click", appleCard);
+
+
+function IOS (){
+	window.open("https://www.apple.com/ios/ios-15/");
+}
+iOS.addEventListener("click", IOS);
+
+
+function shop (){
+	window.open("https://www.apple.com/shop/buy-iphone");
+}
+Shop.addEventListener("click", shop);
+
+
+
+
+
+function learnmore (){
+	window.open("https://www.apple.com/shop/buy-iphone");
+}
+learnmore1.addEventListener("click", learnmore);
+learnmore2.addEventListener("click", learnmore);
+
+
+function buy (){
+	window.open("https://www.apple.com/shop/buy-iphone/iphone-13-pro");
+}
+button.addEventListener("click", buy);

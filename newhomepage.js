a554fd5dd8828fb8f547815a29ec696ba196fb48
Leshovsky Brynn <815990@stma.k12.mn.us>
2022-05-16 12:43:22 -0500

    makeover monday

--- newhomepage.js
+++ newhomepage.js
@@ -1,1 +1,19 @@
-// JavaScript Document
+var facebook = document.getElementById("facebook");
+var twitter = document.getElementById("twitter");
+var rss = document.getElementById("rss");
+
+function Facebook (){
+	window.open("https://www.facebook.com/StMichaelAlbertvillePublicSchools885/");
+}
+facebook.addEventListener("click", Facebook)
+	
+function Twitter (){
+	window.open("https://twitter.com/STMASUPT");
+}
+twitter.addEventListener("click", Twitter)
+
+function Rss (){
+	window.open("https://mn50000079.schoolwires.net/site/Default.aspx?PageType=4&DomainID=4&ModuleInstanceID=6709&PageID=1");
+}
+rss.addEventListener("click", Rss)
+	
